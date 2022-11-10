24f7beb917fc59e73853b33045f0030e233cf95a
huy <doanhuyxh@gmail.com>
2022-11-10 23:35:32 +0700

    base

--- resources/views/layout/about.blade.php
+++ resources/views/layout/about.blade.php
@@ -3,5 +3,4 @@
 @section('content')
     <h1> This is about page </h1>
 
-    <img src="{{ asset('storage/myhc_94121.jpg') }}" alt="Ban">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\PagesController;
+
 
 
 Route::get('/', [PagesController::class, 'index']);
