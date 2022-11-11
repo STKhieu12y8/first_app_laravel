d287304363c685cca829d2e5af7d62d1ec89d807
huy <doanhuyxh@gmail.com>
2022-11-11 14:21:41 +0700

    add sacss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
     <title>Trang chủ</title>
 </head>
 
 <body>
     @include('layout.header')
-    <div>
+    <div class="container">
         @yield('content')
     </div>
     @include('layout.footer')
