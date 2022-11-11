deddb97d306fa5a6bd0ce5d4b40630f96677fc33
huy <doanhuyxh@gmail.com>
2022-11-12 01:15:45 +0700

    sql connection

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('index')
+@section('content')
+
+<h1>This is Post Pages</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
-
+use App\Http\Controllers\PostsController;
 
 
 Route::get('/', [PagesController::class, 'index']);
@@ -10,3 +10,5 @@ Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
 
 Route::get('/trangchu', [PagesController::class, 'trangchu']);
+
+Route::get('/posts', [PostsController::class, 'index']);
